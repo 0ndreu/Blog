3f363f4c4e18490eb51beba5a2b0e8b4cd961514
0ndreu <andrey.artyushov@yandex.ru>
2019-06-07 17:53:31 +0300

    Summernote for admin added

--- MyBlog/news/admin.py
+++ MyBlog/news/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 from news.models import Tag, Category, News
 
+
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text', 'text_min')
+
+
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
 
