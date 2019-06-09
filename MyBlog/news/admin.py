bf0ea1524bc2924b88ead577b380531a66f9323e
0ndreu <andrey.artyushov@yandex.ru>
2019-06-09 17:05:35 +0300

    some admin panel new settings

--- MyBlog/news/admin.py
+++ MyBlog/news/admin.py
@@ -6,16 +6,21 @@ from news.models import Tag, Category, News, Comments
 
 
 class NewsAdmin(SummernoteModelAdmin):
+    """
+    в поле, где применяется ForeignKey или ManyToMany, надо явное указание поля, по которому надо искать
+    """
+    list_display = ('title', 'user', 'created')
+    list_editable = ('user', )          # редактировать поле прямо при выводе статей в админке
     summernote_fields = ('text', 'text_min')
+    list_filter = ('user', 'created', )
+    search_fields = ['title', 'user__username']     # инструкция в начале
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'created', 'moderation')     # порядок отображения в админке
 
 
 admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(News, NewsAdmin)
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'new', 'created', 'moderation')     # отображение в админке
-
-
 admin.site.register(Comments, CommentAdmin)
